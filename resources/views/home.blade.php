9a9bd5061468bbfc239614e652e36ab56d45a90a
= <=>
2022-08-24 16:06:34 +0530

    Project uploaded

---
+++ resources/views/home.blade.php
@@ -0,0 +1,573 @@
+
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="cache-control" content="no-cache" />
+<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+<meta http-equiv="Pragma" content="no-cache" />
+<title>DigiMasters</title>
+<link rel="shortcut icon" type="image/png" href="">
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Best Website Development Company in Panipat, Haryana - Amarka" />
+<meta property="og:description" content="Amarka is a Website Designing and Development Company in Haryana, Panipat. Provide SEO, SMM, Graphic Designing and Digital Marketing Service Also in Haryana, Panipat." />
+<meta property="og:url" content="https://amarka.in/" />
+<meta property="og:site_name" content="Amarka" />
+<meta property="og:image" content="images/favicon.png" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="Best Website Development Company in Panipat, Haryana - Amarka" />
+<meta name="twitter:description" content="Amarka is a Website Designing and Development Company in Haryana, Panipat. Provide SEO, SMM, Graphic Designing and Digital Marketing Service Also in Haryana, Panipat." />
+<meta name="twitter:image" content="images/favicon.png" />
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet" type="text/css" />
+<link href="css/owl.carousel.css" rel="stylesheet">
+<link href="css/animate.css" rel="stylesheet">
+<link href="bootstrap_5/css/bootstrap.min.css" rel="stylesheet">
+<link href="css/style1.css" rel="stylesheet">
+<link href="css/responsive.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css" rel="stylesheet"> </head>
+<body>
+<div class="header_area clearfix" id="back-top-here">
+<nav class="navbar fixed-top navbar-expand-lg ">
+<div class="container">
+<a class="navbar-brand" href="index.html"><img src="images/logo.png"></a>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarsExample07XL">
+<ul class="navbar-nav">
+
+<li class="nav-item">
+<a class="nav-link btn_menu" href="index.html#contact">Enroll Now</a> 
+</li>
+<li class="nav-item">
+<a class="nav-link btn_menu" href="https://www.upwork.com/freelancers/ankurkumar08" target="_blank">Login</a>
+</li>
+</ul>
+</div>
+</div>
+</nav>
+</div>
+<div style="clear:both"></div> <section class="slider padding_section">
+<div class="container clearfix">
+<div class="col-sm-6 col_float">
+<div class="slider_text">
+<p class=" wow  fadeIn" data-wow-duration="800ms" data-wow-delay="200ms"><span>Education That Matters</span></p>
+<h2 class=" wow  zoomIn" data-wow-duration="800ms" data-wow-delay="400ms">Digital Entrepreneurship Bundles.</h2>
+<p class=" wow  fadeIn" data-wow-duration="800ms" data-wow-delay="600ms">From Start To End.</p>
+<h4 class=" wow  fadeIn" data-wow-duration="800ms" data-wow-delay="600ms">How You Will Grow?</h4>
+<p class=" wow  fadeIn" data-wow-duration="800ms" data-wow-delay="600ms">Choose from the wide range of skillful courses to crack the ultimate code of success in the social media world.
+</p>
+<a href="https://www.upwork.com/freelancers/ankurkumar08" target='_blank' class="own_btn click wow  fadeIn" data-wow-duration="800ms" data-wow-delay="600ms">Enroll Now</a>
+</div>
+</div>
+<div class="col-sm-6 col_float">
+<div class="sl_img">
+<img src="images/crypto_wallet_02.png" class="img-responsive">
+</div>
+</div>
+</div>
+</section>
+
+<section class="method_section padding_section" id='about'>
+<div class="container">
+<div class="clearfix row">
+<div class="col-sm-7 col_float col_float_right">
+<div class="title_set text-left">
+<h3>About <span class="color">DigiMasters</span></h3>
+<div class="title_btm"><img src="images/watermark.png" class="img-responsive"></div>
+<p>Digi Masters is an ed-tech platform.</p>
+
+<p>Since August 11, 2021, we've been assisting learners of all ages in learning the trendiest courses in any field with diversified bundles. DigiMasters keeps culture and learning at the root of everything it does.</p>
+
+<p>We’re entrenched in creating a difference. Therefore, we strive to make a distinction in the way people approach entrepreneurship and education.</p>
+
+<p>We deliver courses and bundles designed, created, and produced with real-world statistical data input for the folks we highly believe in.</p>
+
+</div>
+</div>
+<div class="col-sm-1 col_float"></div>
+<div class="col-sm-4 col_float">
+<div class="process_mains">
+<img src="images/info.png" class="img-responsive">
+</div>
+</div>
+</div>
+</section>
+
+<section class="blockchain_pro padding_section" id="about">
+<div class="container">
+<div class="about_inner row">
+<div class="col-md-6">
+<div class="about_contact wow  fadeInUp" data-wow-duration="800ms" data-wow-delay="200ms">
+<div class="title_set">
+<h3>What Others Have To Say About 
+<br><span class="color"> DigiMasters</span></h3>
+<div class="title_btm"><img src="images/watermark.png" class="img-responsive"></div>
+</div>
+<p>DigiMasters has been a life-changing platform for thousands of Indians. Browse what people have to say about us.</p>
+
+<p>The interactive training system and support system by the organization has made my last year more productive than I imagined. I started with zero knowledge or experience in online marketing however, the journey so far was full of learnings and exposure to real-world marketing and business. </p>
+
+
+</div>
+</div>
+<div class="col-md-6">
+<img src="images/cloxkchain.png" class="img-responsive">
+</div>
+</div>
+</div>
+</section>
+<section class="services_section padding_section" id="services">
+<div class="ani_img">
+<img src="images/png-img-04.png" class="img-responsive">
+</div>
+<div class="container">
+<div class="title_set text-center">
+<h3>How<span class="color"> DigiMasters Works</span></h3>
+<div class="title_btm"><img src="images/watermark.png" class="img-responsive"></div>
+</div>
+<div class="services_inner">
+<div class="row clearfix">
+<div class="col-lg-4 col-md-6 col">
+<div class="single_service wow  fadeInUp" data-wow-duration="1000" data-wow-delay="200ms">
+<div class="service_bg">
+<img src="images/service/1.png" alt="">
+<h4>Different Bundles and Trending Skills
+</h4>
+</div>
+<div class="service_content">
+<p>Explore the million possibilities of getting in touch with all the trending skills in the market.</p>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col">
+<div class="single_service wow  fadeInUp" data-wow-duration="1000" data-wow-delay="400ms">
+<div class="service_bg">
+<img src="images/service/2.png" alt="">
+<h4>Learn
+</h4>
+</div>
+<div class="service_content">
+<p>Learn from trainers who have real-time expertise in their respective fields.
+</p>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col">
+<div class="single_service wow  fadeInUp" data-wow-duration="1000" data-wow-delay="600ms">
+<div class="service_bg">
+<img src="images/service/3.png" alt="">
+<h4>Inspire </h4>
+</div>
+<div class="service_content">
+<p>Inspire people close to you and help make a tangible difference in society.
+
+</p>
+</div>
+</div>
+</div>
+
+
+
+</div>
+</div>
+</section>
+
+<section class="profolio_section padding_section" id="about">
+<div class="container">
+<div class="title_set text-center">
+<h3><span class="color"> Popular Courses
+
+ </span></h3>
+<div class="title_btm"><img src="images/watermark.png" class="img-responsive"></div>
+</div>
+</div>
+<div class="container-fluid">
+<div class="por_mul">
+<div class="services_inner text-center">
+<div class="owl-carousel owl-theme">
+<div class="item">
+<a href='https://sandvegascasino.club/' target="_blank"><img src="images/portfolio/bm.png" class="img-responsive"> </a>
+</div>
+<div class="item">
+<a href='https://aftrmrkt.io/' target="_blank"><img src="images/portfolio/mm.jpg" class="img-responsive"> </a>
+</div>
+<div class="item">
+<a href='https://nft.316vc.com/' target="_blank"><img src="images/portfolio/tm.png" class="img-responsive"> </a>
+</div>
+<div class="item">
+<a href='https://coffeejunkieclub.com/' target="_blank"><img src="images/portfolio/im.png" class="img-responsive"> </a>
+</div>
+<div class="item">
+<a href='https://gangsterpunks.co/' target="_blank"><img src="images/portfolio/fm.png" class="img-responsive"> </a>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</section>
+<section class="skills_section services_section padding_section" id="skills">
+<div class="ani_img">
+<img src="images/png-img-04.png" class="img-responsive">
+</div>
+<div class="container">
+<div class="title_set text-center">
+<h3><span class="color"> Trending Skills</span></h3>
+<div class="title_btm"><img src="images/watermark.png" class="img-responsive"></div>
+</div>
+<div class="services_inner text-center">
+<div class="owl-carousel owl-theme">
+<div class="item">
+<img src="images/skills/Ethereum-ETH-icon.png" class="img-responsive">
+<h4>Digital Marketing</h4>
+</div>
+<div class="item">
+<img src="images/skills/2.png" class="img-responsive">
+<h4>Web Development</h4>
+</div>
+<div class="item">
+<img src="images/skills/3.png" class="img-responsive">
+<h4>SEO</h4>
+</div>
+<div class="item">
+<img src="images/skills/4.png" class="img-responsive">
+<h4>SMM</h4>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<section class="which_projects_section padding_section" id="contact">
+<div class="container">
+<div class="row clearfix">
+<div class="col-lg-7 col-md-7 col_pro">
+<div class="projects_here">
+<div class="title_set">
+<h3>History is Boring<br>But numbers aren't!</h3>
+<div class="title_btm"><img src="images/watermark.png" class="img-responsive"></div>
+</div>
+<div class="row clearfix">
+<div class="col-lg-6 col-md-6 col_pro">
+<div class="single_project wow  fadeInUp" data-wow-duration="1000" data-wow-delay="200ms">
+<div class="project_bg">
+<i class="fas fa-link"></i>
+</div>
+<div class="project_content">
+<h4>Courses</h4>
+<p>100+</p>
+</div>
+</div>
+</div>
+<div class="col-lg-6 col-md-6 col_pro">
+<div class="single_project wow  fadeInUp" data-wow-duration="1000" data-wow-delay="200ms">
+<div class="project_bg">
+<i class="fas fa-link"></i>
+</div>
+<div class="project_content">
+<h4>Students Enrolled</h4>
+<p>10k+ </p>
+</div>
+</div>
+</div>
+<div class="col-lg-6 col-md-6 col_pro">
+<div class="single_project wow  fadeInUp" data-wow-duration="1000" data-wow-delay="200ms">
+<div class="project_bg">
+<i class="fas fa-link"></i>
+</div>
+<div class="project_content">
+<h4>Live Training</h4>
+ <p>200+</p>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+<div class="col-lg-5 col-md-5 col_pro">
+<div class="contact_here">
+<div class="title_set">
+<h3>Join Us Now!</h3>
+</div>
+<form method="post">
+<div class="mb-3">
+<input type="email" class="form-control" id="email" name="email" placeholder="Email-Id">
+<label id='name-error' class='error' for='name'>&nbsp;</label>
+</div>
+<div class="mb-3">
+<input type="email" class="form-control" id="email" name="email" placeholder="Confirm Email">
+<label id='email-error' class='error' for='email'>&nbsp;</label>
+</div>
+<div class="mb-3">
+<input type="text" class="form-control" id="mobile" name="mobile" placeholder="Enter Mobile Number">
+<label id='mobile-error' class='error' for='mobile'>&nbsp;</label>
+</div>
+<div class="mb-3">
+<input type="password" class="form-control" id="password" name="password" placeholder="Password">
+<label id='mobile-error' class='error' for='mobile'>&nbsp;</label>
+</div>
+<div class="mb-3">
+<input type="text" class="form-control" id="referal_code" name="referal_code" placeholder="Referral Code">
+<label id='mobile-error' class='error' for='mobile'>&nbsp;</label>
+</div>
+
+<div class="mb-3">
+<select name="course" id="" class="form-control">
+  <option selected>Select Course</option>
+  <option value="option1">option1</option>
+  <option value="option2">option2</option>
+  <option value="option3">option3</option>
+  <option value="option4">option4</option>
+  <option value="option5">option5</option>
+</select>
+</div>
+<div class="ser_btn">
+<button type="button" class="handle_btn" id="submitSupport"><span><i class="fas fa-long-arrow-alt-right"></i> Submit</span></button>
+</div>
+</form>
+</div>
+</div>
+</div>
+
+</div>
+</section>
+<div class="top_slide">
+<a href="index.html#" class="back-to-top d-flex align-items-center justify-content-center"><i class="fa fa-angle-double-up" aria-hidden="true"></i></a>
+</div>
+
+<footer class="bg-dark text-light">
+<div class="container">
+<div class="f-items default-padding">
+<div class="row text-center">
+<div class="col-lg-12 col-md-12 item">
+<div class="f-item link">
+
+</ul>
+</div>
+</div>
+<div class="col-lg-12 col-md-12 item aa_herer">
+<div class="f-item contact">
+<div class="address">
+<ul>
+<li class="col-lg-4 col-md-4 col">
+<p>DigiMasters Learner Support</p>
+<strong>+9190535-17595.</strong>
+<br>
+<p>Timings</p>
+<strong>(We are available on Monday to Friday 9:00 am to 6:00 pm)
+
+</strong>
+</li>
+<li class="col-lg-4 col-md-4 col">
+<p>Email</p>
+<strong><a href="">info@zoefact.com</a></strong>
+</li>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="footer-bottom">
+<div class="container">
+<div class="row align-center">
+<div class="col-lg-4">
+<p>© Copyright 2022. All Rights Reserved</p>
+</div>
+<div class="col-lg-4 text-center logo">
+<a href="index.html#"><img src="images/logo.png" alt="Logo"></a>
+</div>
+
+</div>
+</div>
+</div>
+</footer>
+<script data-cfasync="false" src="cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="js/jquery.js"></script>
+<script src="bootstrap_5/js/bootstrap.bundle.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script type="text/javascript">
+      
+$(document).ready(function() {
+  var s = $(".header_area");
+  var pos = s.position();
+  $(window).scroll(function() {
+    var windowpos = $(window).scrollTop();
+    if (windowpos >= pos.top & windowpos >=50) {
+      s.addClass("scroll_stick");
+    } else {
+      s.removeClass("scroll_stick");
+    }
+  });
+  });
+
+$('.slider .owl-carousel').owlCarousel({
+loop:true,
+margin:0,
+autoplay:true,
+autoplayTimeout:3000,
+autoplayHoverPause:true,
+nav:true,
+responsive:{
+  0:{
+    items:1
+  },
+  600:{
+    items:1
+  },
+  1000:{
+    items:1
+  }
+}
+});
+$('.skills_section .owl-carousel').owlCarousel({
+loop:true,
+margin:0,
+autoplay:true,
+autoplayTimeout:3000,
+autoplayHoverPause:true,
+nav:true,
+responsive:{
+  0:{
+    items:2
+  },
+  600:{
+    items:3
+  },
+  1000:{
+    items:4
+  }
+}
+});
+$('.profolio_section .owl-carousel').owlCarousel({
+loop:true,
+margin:15,
+autoplay:true,
+autoplayTimeout:3000,
+autoplayHoverPause:true,
+nav:true,
+responsive:{
+  0:{
+    items:2
+  },
+  600:{
+    items:3
+  },
+  1000:{
+    items:5
+  }
+}
+});
+
+</script>
+
+<script src="js/wow.min.js"></script>
+
+<script src="js/main.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+<script src="js/jquery.validate.js"></script>
+<script src="js/additional-methods.min.js"></script>
+<script>
+        var Form 			= $('#support');
+        var btnName 		= $('#submitSupport').html();
+        var loader      	= '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>';
+         $("#submitSupport").click(function(event){
+            
+              var sendValidate	= $("#support").validate(
+              {
+                 rules:
+                {
+                  name:
+                  {
+                    required	: true,
+                  },
+                    email:{
+                      required 	: true,
+                      email		: true,
+                  },
+                  mobile:{
+                      required 	: true,
+                      number	: true,
+                  }
+                },
+                messages:
+                {
+                  name:
+                  {
+                    required	: "Name is required.",
+                  },
+                    email:{
+                      required	: "Email is required.",
+                      email		: "Please enter valid email.",
+                  },
+                  mobile:{
+                      required	: "Mobile number is required.",
+                      number	: "Please enter valid mobile number.",
+                  }
+                }
+             });
+            if($("#support").valid()==true)
+            {
+                var str = Form.serialize();
+                $.ajax(
+                {
+                    type: "POST",
+                    url:  Form.attr('action'),
+                    data: str,
+                    beforeSend: function ()
+                    {
+                       // $('#submitSupport').html(loader);
+                        //$('#submitSupport').attr('disabled',true);
+                    },
+                    success: function( msg ) {
+                        var resp= JSON.parse(msg);
+                        if(("cf" in resp)){
+                          $('.Csrf').val(resp.cf);
+                        }
+                        if(resp.messageType=='comman')
+                        {
+                            if(resp.status==1)
+                            {
+                              Form.find(':input').not('.submit,input:hidden').val('');
+                                $('#change-password .error').text('');
+                                var title="Success";
+                            }
+                            else{
+                              var title="Error";
+                            }
+                          
+                          swal({
+                                title: title,
+                                text: resp.message,
+                                type: title.toLowerCase(),
+                                showCancelButton: false,
+                                confirmButtonClass: "btn-danger",
+                                confirmButtonText: "Ok",
+                                closeOnConfirm: false
+                            });
+                        }
+                        else
+                        {
+                            $.each(resp.fields, function(key, value){
+                                $("#"+key+'-error').text(value);
+                            });
+                        }
+                    },
+                    complete: function (xhr, status, error_thrown) {
+                       // $('#submitSupport').html(btnName);
+                       // $('#submitSupport').attr('disabled',false);
+                    }
+                });
+            }
+        });
+      </script>
+</body>
+</html>
